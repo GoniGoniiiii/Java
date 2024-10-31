709932ac79354d99544d5ef168dfd02de6dedb7e
YouGoWoon <rhdnsd409@naver.com>
2024-10-31 23:52:34 +0900

    연산자, 단항연산자

---
+++ src/chap03_operator/OperatorEx3.java
@@ -0,0 +1,10 @@
+package chap03_operator;
+
+public class OperatorEx3 {
+    public static void main(String[] args) {
+        int j=5 , i=5;
+        System.out.println(i++);
+        System.out.println(++j);
+        System.out.println("i = "+ i + "j = "+j);
+    }
+}
