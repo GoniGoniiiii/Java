6ff7604bea8693a99208b196cfcf335f823df259
YouGoWoon <rhdnsd409@naver.com>
2024-11-19 00:03:46 +0900

    if문 예제3

---
+++ src/chap04_if/FlowEx3.java
@@ -0,0 +1,26 @@
+package chap04_if;
+
+import java.util.Scanner;
+
+public class FlowEx3 {
+    public static void main(String[] args) {
+        int score=0;
+        char grade =' ';
+
+        System.out.print("점수를 입력 : ");
+        Scanner sc = new Scanner(System.in);
+        score = sc.nextInt();
+
+        if(score>=90){
+            grade = 'A';
+        }else if(score>=80){
+            grade = 'B';
+        }else if(score>=70){
+            grade = 'C';
+        }else{
+            grade = 'D';
+        }
+
+        System.out.print("당신의 학점은 "+grade);
+    }
+}
